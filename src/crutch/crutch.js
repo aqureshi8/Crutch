1bd13ef18b4ff53d9e5d9817f6e7cba69308bf9d
Ahsan <ahsan.qureshi8@gmail.com>
2021-02-27 01:39:32 -0500

    can toggle the standups but they all have the same tasks... why?

--- src/crutch/crutch.js
+++ src/crutch/crutch.js
@@ -18,6 +18,11 @@ class Crutch extends React.Component {
   loadStandUps() {
     //LOAD LAST 10 Stand ups
     let standUps = [];
+    for(var x = 9; x > 0; x--) {
+      let xDaysAgo = new Date();
+      xDaysAgo.setDate(new Date().getDate() - x);
+      standUps.push(<StandUp date={xDaysAgo} />);
+    }
     //Current standup
     standUps = standUps.concat([
       <StandUp date={new Date(Date.now())}/>
@@ -25,18 +30,34 @@ class Crutch extends React.Component {
     return standUps;
   }
 
+  toggleCurrentStandup(count) {
+    let currentStandUp = this.state.currentStandUp;
+    this.setState({
+      currentStandUp: currentStandUp + count
+    });
+  }
+
   render() {
     const standUps = this.state.standUps;
-    const displayStandUp = standUps[this.state.currentStandUp];
+    const currentStandUp = this.state.currentStandUp;
+    const displayStandUp = standUps[currentStandUp];
     const title = displayStandUp.props.date.toDateString();
+    const leftToggleDisabled = currentStandUp - 1 < 0
+    const rightToggleDisabled = currentStandUp + 1 >= standUps.length;
     return (
       <div className="crutch">
         <div className="dateToggle">
-          <div className="dateToggleButton">
+          <div
+            className={"dateToggleButton" + (leftToggleDisabled ? " disabledToggle" : "")}
+            onClick={() => this.toggleCurrentStandup(-1)}
+          >
             <FontAwesomeIcon icon={faCaretSquareLeft} />
           </div>
           <div className="title">{title}</div>
-          <div className="dateToggleButton">
+          <div
+            className={"dateToggleButton" + (rightToggleDisabled ? " disabledToggle" : "")}
+            onClick={() => this.toggleCurrentStandup(1)}
+          >
             <FontAwesomeIcon icon={faCaretSquareRight} />
           </div>
         </div>
